ea23702c314c38f8870e25ad1b8155ab2923f1f0
Timothy jan <jan.timothy@gmail.com>
2024-08-01 11:16:56 -0700

    Added Validation Rules to the Movie model

--- RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -6,13 +6,29 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 1)]
         public string? Title { get; set; }
+
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
-        public string? Genre { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(30)]
+        public string Genre { get; set; } = string.Empty;
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
         public string Rating { get; set; } = string.Empty;
     }
 }
